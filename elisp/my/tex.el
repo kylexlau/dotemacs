afea9874c0d8124c222a61cf8a9b45f324c29061
kyle <kyle@eeepc.(none)>
2010-01-06 09:20:19 +0800

    etexshow configuration.
    etexshow is ConTeXt's documentation system for Emacs.

--- elisp/my/tex.el
+++ elisp/my/tex.el
@@ -1,7 +1,28 @@
+;; etexshow
+(autoload 'etexshow "etexshow"  "Browser for ConTeXt commands." t)
+
+(setq etexshow-xml-files-alist 
+      '(("~/prj/emacs/elisp/etexshow/cont-en.xml" . "/tmp/cont-en.cache")))
+
+(setq etexshow-comment-file   "~/.cont-en-comments.xml" )
+
+(global-set-key [f7] 'etexshow)
+(add-hook 'etexshow-mode-hook '(lambda () 
+				 (local-set-key [f7] 'etexshow-quit)))
+
+;; auctex
 (load "auctex.el" nil t t)
 (load "preview-latex.el" nil t t)
 (add-to-list 'auto-mode-alist '("\\.tex\\'" . LaTeX-mode))
 
 (add-hook 'LaTeX-mode-hook
 	  '(lambda() 
-	     (outline-minor-mode 1)))
+	     (outline-minor-mode 1)
+	     (auto-fill-mode 1)
+	     ))
+
+(add-hook 'context-en-mode-hook
+	  '(lambda() 
+	     (outline-minor-mode 1)
+	     (auto-fill-mode 1)
+	     ))
