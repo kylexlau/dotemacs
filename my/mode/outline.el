ff8616f94404a11d47296acce8ab600dc29c6c2b
kyle x lau <kylexlau@gmail.com>
2011-04-04 19:16:46 +0800

    fix hook bugs for ConTeXt-en-mode and outline-minor-mode.

--- my/mode/outline.el
+++ my/mode/outline.el
@@ -1,3 +1,7 @@
 (add-hook 'outline-mode-hook
 	  (lambda ()
+	    (define-key outline-mode-map "\t" 'org-cycle)))
+
+(add-hook 'outline-minor-mode-hook
+	  (lambda ()
 	    (define-key outline-minor-mode-map "\t" 'org-cycle)))
