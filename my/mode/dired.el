51de9f08f3484b2447ae5e9f367388025ad3c790
kyle x lau <kylexlau@gmail.com>
2011-04-06 20:22:48 +0800

    fix dired-omit-mode setting.

--- my/mode/dired.el
+++ my/mode/dired.el
@@ -1,1 +1,9 @@
-(require 'dired-x)
+;; Load Dired X when Dired is loaded.
+(add-hook 'dired-load-hook '(lambda () (require 'dired-x)))
+
+;; Enable toggling of uninteresting files.
+(setq dired-omit-files-p t)
+
+(setq dired-omit-files
+          (concat dired-omit-files "\\|^\\..+$"))
+
