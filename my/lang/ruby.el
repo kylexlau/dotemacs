50698af3c711aeb843042da5ebd329575b24ebcb
kyle x lau <kylexlau@gmail.com>
2011-04-07 18:51:10 +0800

    add file mapping for ruby mode.

--- my/lang/ruby.el
+++ my/lang/ruby.el
@@ -1,6 +1,19 @@
 ;; ruby-mode
-(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+
+;; File Mappings
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.thor$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.js.rjs$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.xml.builder$" . ruby-mode))
 
 ;; ruby-electric-mode
 (add-hook 'ruby-mode-hook 'ruby-electric-mode)
