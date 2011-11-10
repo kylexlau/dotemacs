18cc418235d71ed6746d396d259d962e8e953849
kyle x lau <kylexlau@gmail.com>
2011-11-11 00:29:36 +0800

    custom-file, tsdh-dark theme.

---
+++ my/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(ecb-layout-window-sizes (quote (("left8" (ecb-directories-buffer-name 0.22857142857142856 . 0.2857142857142857) (ecb-sources-buffer-name 0.22857142857142856 . 0.22857142857142856) (ecb-methods-buffer-name 0.22857142857142856 . 0.2857142857142857) (ecb-history-buffer-name 0.22857142857142856 . 0.17142857142857143)))))
+ '(ecb-options-version "2.40")
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
