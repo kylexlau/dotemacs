b6aeb743fd0136be92f331517f364653488e6631
kyle x lau <kylexlau@gmail.com>
2011-12-11 20:37:09 +0800

    bind magit-status to C-x g.

--- my/binding.el
+++ my/binding.el
@@ -1,11 +1,16 @@
 (global-set-key [f11] 'my-toggle-fullscreen)
+(global-set-key "\C-x\C-b" 'ibuffer)
 (global-set-key "\C-co" 'speedbar-select-attached-frame)
-(global-set-key (kbd "C-=") 'hippie-expand)
+(global-set-key "\C-c\M-q" 'unfill-paragraph)
 
+;; org-mode
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 (global-set-key "\C-cr" 'org-remember)
 
-(global-set-key "\C-c\M-q" 'unfill-paragraph)
-(global-set-key "\C-x\C-b" 'ibuffer)
+;; git
+(global-set-key "\C-xg" 'magit-status)
+
+;; completion
+(global-set-key (kbd "C-=") 'hippie-expand)
