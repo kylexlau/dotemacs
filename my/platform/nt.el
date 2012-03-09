a6554a379d44a593dd7699a634210d838b95b167
kylexlau <kyle@ly.com>
2012-03-09 16:22:44 +0800

    use package.el to manage elisp packages under windows platform.

--- my/platform/nt.el
+++ my/platform/nt.el
@@ -1,4 +1,4 @@
-(add-to-list 'load-path (concat emacs-el-dir "/elisp"))
+;(add-to-list 'load-path (concat emacs-el-dir "/elisp"))
 
 (set-frame-font "Consolas-10.5")
 
@@ -10,3 +10,6 @@
 ;; fix ~/.emacs.d/server is unsafe on w32
 (when (and (= emacs-major-version 23) (equal window-system 'w32))
   (defun server-ensure-safe-dir (dir) "Noop" t))
+
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
