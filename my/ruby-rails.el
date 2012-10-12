9dcaa7fe79b81132871db3e008abfe8091d3a6eb
kyle <kylexlau@gmail.com>
2012-10-12 16:27:08 +0800

    Optimize ruby mode hook setting.
    If a mode is not installed, don't use it.

--- my/ruby-rails.el
+++ my/ruby-rails.el
@@ -1,5 +1,9 @@
 ;; ruby mode
-(add-hook 'ruby-mode-hook 'ruby-electric-mode)
+(if (functionp 'ruby-electric-mode)
+    (add-hook 'ruby-mode-hook 'ruby-electric-mode))
+
+(if (functionp 'textmate-mode)
+    (add-hook 'ruby-mode-hook 'textmate-mode))
 
 (add-hook 'ruby-mode-hook
 	  '(lambda ()
@@ -7,5 +11,4 @@
 	     (setq outline-regexp " *\\(def \\|class\\|module\\)")
 	     (define-key ruby-mode-map "\t" 'org-cycle)
 	     (outline-minor-mode 1)
-	     (textmate-mode 1)
 	     ))
